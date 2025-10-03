3c6b26ec6e723625cbaff0fbf0ccdac21cec20aa
mebusw <mebusw@163.com>
2025-10-03 14:09:21 +0800

    feat: 自定义多个css

--- convert-html.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-pandoc README.md -o output.html \
-  --template=custom_template.html 
-
-echo "转换完成：output.html"
-open output.html
--- convert-pdf.sh
+++ convert-pdf.sh
@@ -8,7 +8,8 @@
 #   --pdf-engine=wkhtmltopdf
 
 ## 方式二：先生成 HTML，再用 weasyprint 转 PDF
-pandoc README.md -o output.pdf \
+pandoc README.md -o output.html \
+  --css=custom_style.css \
   --template=custom_template.html 
 echo "生成完成：output.html"
 weasyprint output.html output.pdf
