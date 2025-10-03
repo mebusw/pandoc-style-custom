a1b5ea2be240b2d59645baa72fa9f8ebde912fbe
mebusw <mebusw@163.com>
2025-10-03 12:13:56 +0800

    feat: 先生成 HTML，再用 weasyprint 转 PDF

--- convert-pdf.sh
+++ convert-pdf.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
+
+## 方式一：直接生成 PDF
+# pandoc README.md -o output.pdf \
+#   --from markdown \
+#   --to html5 \
+#   --template=custom_template.html \
+#   --pdf-engine=wkhtmltopdf
+
+## 方式二：先生成 HTML，再用 weasyprint 转 PDF
 pandoc README.md -o output.pdf \
   --template=custom_template.html 
+echo "生成完成：output.html"
+weasyprint output.html output.pdf
 
 echo "转换完成：output.pdf"
 open output.pdf
